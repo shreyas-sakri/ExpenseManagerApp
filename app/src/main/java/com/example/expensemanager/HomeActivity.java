688a8a940adf64bb47b545965d722fcd9ee22970
shreyas-sakri <shreyassakri9860@gmail.com>
2021-05-20 14:05:31 +0530

    Revert "Implemented Navigation Drawer"
    This reverts commit c0711d80abf27fedf877516465b5498752c67c1a.

--- app/src/main/java/com/example/expensemanager/HomeActivity.java
+++ app/src/main/java/com/example/expensemanager/HomeActivity.java
@@ -1,90 +1,14 @@
 package com.example.expensemanager;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toolbar;
 
-import com.google.android.material.navigation.NavigationView;
+public class HomeActivity extends AppCompatActivity {
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Expense Manager");
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
-                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
-        );
-
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView=findViewById(R.id.naView);
-        navigationView.setNavigationItemSelectedListener(this);
-
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
-        if (drawerLayout.isDrawerOpen(GravityCompat.END)){
-            drawerLayout.closeDrawer(GravityCompat.END);
-        }else {
-            super.onBackPressed();
-        }
-
-    }
-    public void displaySelectedListner(int itemId){
-        Fragment fragment=null;
-
-        switch (itemId)
-        {
-            case R.id.dashboard:
-                break;
-
-            case R.id.income:
-                break;
-
-            case R.id.expense:
-                break;
-        }
-
-        if (fragment!=null)
-        {
-            FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.main_frame,fragment);
-            ft.commit();
-        }
-
-        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
-        drawerLayout.closeDrawer(GravityCompat.START);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        displaySelectedListner(item.getItemId());
-        return true;
     }
 }
