eb386a280a22979e75abcd22d6363a7dfe85b862
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2021-05-21 15:06:27 +0530

    till video 13

--- app/src/main/java/com/example/expensemanager/HomeActivity.java
+++ app/src/main/java/com/example/expensemanager/HomeActivity.java
@@ -9,16 +9,28 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Switch;
 //import android.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    //Fragment
+
+    private DashBoardFragment dashBoardFragment;
+    private IncomeFragment incomeFragment;
+    private ExpenseFragment expenseFragment;
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +38,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar=findViewById(R.id.my_toolbar);
         toolbar.setTitle("Expense Manager");
         setSupportActionBar(toolbar);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationBar);
+        frameLayout=findViewById(R.id.main_frame);
 
         DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
@@ -38,6 +53,44 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView=findViewById(R.id.naView);
         navigationView.setNavigationItemSelectedListener(this);
 
+        dashBoardFragment=new DashBoardFragment();
+        incomeFragment=new IncomeFragment();
+        expenseFragment=new ExpenseFragment();
+
+        setFragment(dashBoardFragment);
+
+
+       bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               switch(item.getItemId()){
+
+                   case R.id.dashboard:
+                       setFragment(dashBoardFragment);
+                       bottomNavigationView.setItemBackgroundResource(R.color.dashboard_color);
+                       return true;
+
+                   case R.id.income:
+                       setFragment(incomeFragment);
+                       bottomNavigationView.setItemBackgroundResource(R.color.income_color);
+                       return true;
+
+                   case R.id.expense:
+                       setFragment(expenseFragment);
+                       bottomNavigationView.setItemBackgroundResource(R.color.expense_color);
+                       return true;
+
+                   default:
+                       return false;
+               }
+           }
+       });
+}
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
@@ -57,12 +110,15 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         switch (itemId)
         {
             case R.id.dashboard:
+                fragment=new DashBoardFragment();
                 break;
 
             case R.id.income:
+                fragment=new IncomeFragment();
                 break;
 
             case R.id.expense:
+                fragment=new ExpenseFragment();
                 break;
 
         }
--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -35,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth=FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }
+
         mDialog=new ProgressDialog(this);
         loginDetails();
     }
