c10c05b05a38a76e5b184f4ac8242cd32130eb97
shreyas-sakri <shreyassakri9860@gmail.com>
2021-05-21 18:21:24 +0530

    implemented floating button animation

--- app/src/main/java/com/example/expensemanager/DashBoardFragment.java
+++ app/src/main/java/com/example/expensemanager/DashBoardFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +60,68 @@ public class DashBoardFragment extends Fragment {
         }
     }
 
+    //Floating Button
+
+    private FloatingActionButton fab_main_btn;
+    private  FloatingActionButton fab_income_btn;
+    private FloatingActionButton fab_expense_btn;
+
+    //Floating button TextView
+
+    private TextView fab_income_txt;
+    private TextView fab_expense_txt;
+    //boolean
+    private boolean isOpen=false;
+
+    //Animation
+
+    private Animation FadeOpen,FadeClose;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+        View myview = inflater.inflate(R.layout.fragment_dash_board, container, false);
+        // Connect Floating button to layout
+        fab_main_btn=myview.findViewById(R.id.fb_main_plus_btn);
+        fab_income_btn=myview.findViewById(R.id.income_Ft_btn);
+        fab_expense_btn=myview.findViewById(R.id.expense_Ft_btn);
+        // connect floating text
+        fab_income_txt=myview.findViewById(R.id.income_ft_text);
+        fab_expense_txt=myview.findViewById(R.id.expense_Ft_btn);
+
+        //Animation connect...
+        FadeOpen= AnimationUtils.loadAnimation(getActivity(),R.anim.fade_open);
+        FadeClose=AnimationUtils.loadAnimation(getActivity(),R.anim.fade_close);
+
+        fab_main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_income_btn.startAnimation(FadeClose);
+                    fab_expense_btn.setClickable(false);
+                    fab_expense_btn.setClickable(false);
+
+                    fab_income_txt.startAnimation(FadeClose);
+                    fab_expense_txt.startAnimation(FadeClose);
+                    fab_income_txt.setClickable(false);
+                    fab_expense_txt.setClickable(false);
+                    isOpen=false;
+                }else {
+                    fab_income_btn.startAnimation(FadeOpen);
+                    fab_income_btn.startAnimation(FadeOpen);
+                    fab_expense_btn.setClickable(true);
+                    fab_expense_btn.setClickable(true);
+
+                    fab_income_txt.startAnimation(FadeOpen);
+                    fab_expense_txt.startAnimation(FadeOpen);
+                    fab_income_txt.setClickable(true);
+                    fab_expense_txt.setClickable(true);
+                    isOpen=true;
+                }
+            }
+        });
+        return myview;
+
     }
 }
